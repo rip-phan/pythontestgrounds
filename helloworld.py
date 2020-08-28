34dffd8bad9852db23c6bc56ffed9b76655cfc15
Rip-Phan <rip@rip-phan.dev>
2020-08-28 06:08:47 -0400

    adding index html and a python greeting file / scratch pad

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+def myfunc():
+	global name
+name="Rip-Phan"
+
+
+quote="""
+bitches know my name
+but they dont
+jokes on them
+"""
+#Python is not monte
+z=5239/2000+800*55
+print(name)
+
+print("5239 divided by 2000 plus 800 multiplied by 55 is equal to", z, ".")
+
+name="Joe"
+age=31
+age2=32
+
+print("Hello, World!!!", name, " is my name!, Don't forget that!")
+print("I'm", age,"--", age2, "years old and I prolly fuck you up!!")
+print(quote)
